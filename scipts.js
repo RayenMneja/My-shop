ac5f56815e3e7c02df43ac4e43d3315d8a561097
RayenMneja <rayenmneja2006@gmail.com>
2024-03-20 23:49:15 +0100

    Update HTML and JS code for search functionality and footer. (generated by blackbox.ai )

--- scipts.js
+++ scipts.js
@@ -40,7 +40,11 @@ let produit=["a5thar 7chich","a5tharna3ne3i","a7mar ch3chou3i","a7mar","asfar mi
             form.addEventListener('submit', e => {
               e.preventDefault()
               fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-              .then(response => alert("Thank you! your form is submitted successfully." ))
+              .then(response => Swal.fire({
+                title: "Good job!",
+                text: "You clicked the button!",
+                icon: "success"
+              }))
               .then(() => { window.location.reload(); })
               .catch(error => console.error('Error!', error.message))
             });
@@ -62,13 +66,30 @@ let produit=["a5thar 7chich","a5tharna3ne3i","a7mar ch3chou3i","a7mar","asfar mi
             let dalite = document.createElement('button');
             dalite.textContent = 'delete';
             dalite.addEventListener('click', () => {
-                cart.splice(cart.indexOf(productId), 1);
-                renderCart();
-                var s= document.getElementById('h3');
-                const v= flous[productId];
-                const t = somme;
-                somme=t-v;
-                s.innerHTML = 'total: dt '+somme;
+              Swal.fire({
+                title: "Are you sure?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, delete it!"
+              }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire({
+                    title: "Deleted!",
+                    text: "Your file has been deleted.",
+                    icon: "success"
+                    })
+                    cart.splice(cart.indexOf(productId), 1);
+                    renderCart();
+                    var s= document.getElementById('h3');
+                    const v= flous[productId];
+                    const t = somme;
+                    somme=t-v;
+                    s.innerHTML = 'total: dt '+somme; 
+                }
+              }); 
             });
     
             row.appendChild(productCell);
@@ -96,14 +117,39 @@ let produit=["a5thar 7chich","a5tharna3ne3i","a7mar ch3chou3i","a7mar","asfar mi
           affichage += `<a class="ch" href="#${compt[i]}">${resultat[i]}</a>`;
         }      
         document.getElementById("resultat").innerHTML = affichage;
+        
       }
       
-   };
-   function btnrecherch(){
+    };
     
-    setTimeout(rechercherProduits(),10000);
-    
-   }
+'use strict'
+
+const menuToggle = document.querySelector('.menu-toggle');
+const bxMenu = document.querySelector('.bx-menu');
+const bxX = document.querySelector('.bx-x');
+
+const navBar = document.querySelector('.navbar');
+
+// --- open menu ---
+
+bxMenu.addEventListener('click', (e)=> {
+    if(e.target.classList.contains('bx-menu')){
+        navBar.classList.add('show-navbar');
+        bxMenu.classList.add('hide-bx');
+        bxX.classList.add('show-bx');
+    }
+})
+
+// --- close menu ---
+
+bxX.addEventListener('click', (e)=> {
+    if(e.target.classList.contains('bx-x')){
+        navBar.classList.remove('show-navbar');
+        bxMenu.classList.remove('hide-bx');
+        bxX.classList.remove('show-bx');
+    }
+})
+
 
 
 
